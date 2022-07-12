a7048ce6ce75ced70ceffe2806b6c0f32016184c
NeysaBan <48760129+NeysaBan@users.noreply.github.com>
2022-07-12 18:27:33 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '470f98ddd95e468eb5108702fd126da3',
+  rootNotionPageId: 'FuckCode-470f98ddd95e468eb5108702fd126da3',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
